0ee3cfa04e167199cccbc548ecd5d9707353ebf9
James Walker <jwalker@scottlogic.com>
2024-06-04 13:50:01 +0100

    Update to Euro 2024

--- functions/constants.js
+++ functions/constants.js
@@ -1,7 +1,7 @@
 const COMPETITION_API = {
   URL: {
     BASE: "https://api.football-data.org/v2/competitions",
-    COMPETITION_CODE: "2167",
+    COMPETITION_CODE: "2018",
     FIXTURES: "matches",
     TEAMS: "teams",
   },
